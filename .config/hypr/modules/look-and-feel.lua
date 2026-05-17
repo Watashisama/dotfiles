90ccc7d3df9c4fd56f49812004f533c835da17c1
Watashisama <denzeru.d@proton.me>
2026-05-17 08:35:45 +0000

    ..

--- .config/hypr/modules/look-and-feel.lua
+++ .config/hypr/modules/look-and-feel.lua
@@ -23,7 +23,7 @@ hl.config({
       enabled      = false,
       range        = 4,
       render_power = 3,
-      color        = 0xee1a1a00,
+      color        = 0x00000000,
     },
     blur             = {
       enabled  = true,
