cbc4b43f0d34c6c314c8779646ad65ca55ed7025
Watashisama <denzeru.d@proton.me>
2026-05-19 20:44:01 +0000

    Update blink-cmp.lua

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -18,7 +18,7 @@ require("blink.cmp").setup({
   appearance = {
     nerd_font_variant = "mono",
   },
-  --
+  
   completion = { documentation = { auto_show = true } },
   sources = {
     default = { "snippets", "lsp", "path", "buffer" },
