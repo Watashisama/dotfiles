8dac1966a3aab416d2c245fd9ec502764b5b589e
Watashisama <denzeru.d@proton.me>
2026-05-30 14:07:12 +0000

    hyprwallpaper?

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -5,21 +5,18 @@
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 
-hl.on("hyprland.start", function()
+hl.on("hyprland.start", function ()
   hl.exec_cmd("hyprctl setcursor Banana-Mod 32")
   hl.exec_cmd("wayle panel start")
-  hl.exec_cmd("hyprpaper")
+  -- hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("nu ~/.config/hypr/scripts/walllpaper.nu")
   hl.exec_cmd("dunst")
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
   hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
   hl.exec_cmd("fcitx5 -d")
   -- hl.exec_cmd( "systemd-inhibit --who='Hyprland config' --why='Disable Lid suspend' --what=handle-lid-switch   --mode=block sleep infinity & echo $! > /tmp/.hyprland-systemd-inhibit-lid")
-end
-)
-
-
-
+end)
 
 -- exec-once = ~/.config/hypr/scripts/wpctl.nu
 -- exec-once = ~/.config/quickshell/scripts/cpu.nu
---
+++ .config/hypr/scripts/walllpaper.nu
@@ -0,0 +1,13 @@
+#!/usr/bin/env nu
+
+echo "hyprpaper &" | bash
+sleep 300ms
+let wallpaper_dir = $env.XDG_CONFIG_DIR | path join wallpapers/
+let wallpapers = (
+  ls $wallpaper_dir
+  | get index
+  | length
+)
+let random_number = random int 1..($wallpapers)
+
+hyprctl hyprpaper wallpaper $"eDP-1, ~/.config/wallpapers/($random_number).png, [cover]"
