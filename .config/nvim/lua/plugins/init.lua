226476712e46584e6b56bcc7a24510e31c5d93b5
Watashisama <denzeru.d@proton.me>
2026-05-31 07:14:33 +0000

    unpacked.nvim?????!?

--- .config/nvim/lua/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-  require("plugins.blink-cmp"), require("plugins.figdet"), require("plugins.git-integration"),
-  require("plugins.markdown"), require("plugins.mason"), require("plugins.mini-hipatterns"),
-  require("plugins.mini-indentscope"), require("plugins.oil"), require("plugins.treesitter"), require("plugins.trouble"),
-  require("plugins.vimtex"), require("plugins.which-key")
-}
